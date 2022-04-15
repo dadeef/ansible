80939027df0a1861aed3d37647640514fdd02384
David Deren <da_deef@mac.com>
2022-04-15 16:57:19 +0200

    get username from script

--- ansible_run.sh
+++ ansible_run.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 echo "lets get things started"
 
-echo "What is the username on the machine you're setting up? "
-read username
-
 echo "What's the Ansible Vault password? "
 read -s vault_password
 echo $vault_password > vault-pass.txt #We need to temporarily store the password in a file because we cannot use the --ask-vault-password with the ansible-pull command
@@ -13,6 +10,6 @@ sudo xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install ansible
 
-sudo ansible-pull -U https://github.com/dadeef/ansible.git --extra-vars "user=$username" --vault-password-file vault-pass.txt
+sudo ansible-pull -U https://github.com/dadeef/ansible.git --extra-vars "user=(id -un)" --vault-password-file vault-pass.txt
 
 rm vault-pass.txt
